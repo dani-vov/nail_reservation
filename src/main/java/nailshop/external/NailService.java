5fb5183c4c19844f90f9e54f0948c330932ad3b3
dani-vov <dani-vov>
2020-04-23 10:49:33 +0900

    소스 수정

--- src/main/java/nailshop/external/NailService.java
+++ src/main/java/nailshop/external/NailService.java
@@ -1,10 +1,12 @@
 package nailshop.external;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Service
 @FeignClient(name = "work", url = "${api.url.work}")
 public interface NailService {
 
